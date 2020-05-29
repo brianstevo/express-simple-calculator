ea3f3bf27e376fd746401b8c128feeca18c742b1
brianstevo <brianaranha76@gmail.com>
2020-05-29 14:56:34 +0530

    express js exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # express-simple-calculator
+studying express js 
+trying to add 2 numbers using express js and body-parser
---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.send("<h1>simple calculator</h1>");
+})
+app.get('/cal',(req,res)=>{
+    res.sendFile(`${__dirname}/index.html`);
+})
+app.post('/cal',(req,res)=>{
+    const a=Number(req.body.num1);
+    const b=Number(req.body.num2);
+    const total=a+b;
+    res.send('sum is: '+total);
+})
+
+const port=5000;
+app.listen(port,()=>{
+    console.log(`listerning on server ${port}`);
+});
