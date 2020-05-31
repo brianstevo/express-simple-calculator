e25dd985cbaca3933eb7ba0b6b2f19e692f79746
brianstevo <brianaranha76@gmail.com>
2020-05-31 10:19:51 +0530

    changed method slightly

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const app=express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     res.send("<h1>simple calculator</h1>");
